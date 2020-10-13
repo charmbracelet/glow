8ed7bb7717eae5ea39233aa18d4c1cc70249ff13
Christian Rocha <christian@rocha.is>
2020-10-12 23:57:08 -0400

    Restore ANSI support to non-TUI Glow on Windows

---
+++ console_windows.go
@@ -0,0 +1,23 @@
+// +build windows
+
+package main
+
+import (
+	"os"
+
+	"golang.org/x/sys/windows"
+)
+
+// enableAnsiColors enables support for ANSI color sequences in Windows
+// default console. Note that this only works with Windows 10.
+func enableAnsiColors() {
+	stdout := windows.Handle(os.Stdout.Fd())
+	var originalMode uint32
+
+	windows.GetConsoleMode(stdout, &originalMode)
+	windows.SetConsoleMode(stdout, originalMode|windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING)
+}
+
+func init() {
+	enableAnsiColors()
+}
