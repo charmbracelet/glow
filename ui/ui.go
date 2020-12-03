322495ca3e3aa68bd991222e908834812441b5db
Christian Rocha <christian@rocha.is>
2020-12-03 17:33:09 -0500

    Correct logic for checking when we're in stashed-only-mode

--- ui/ui.go
+++ ui/ui.go
@@ -49,6 +49,18 @@ type Config struct {
 	GlamourEnabled       bool   `env:"GLOW_ENABLE_GLAMOUR" default:"true"`
 }
 
+func (c Config) showLocalFiles() bool {
+	return c.DocumentTypes.Contains(LocalDoc)
+}
+
+func (c Config) localOnly() bool {
+	return c.DocumentTypes.Equals(NewDocTypeSet(LocalDoc))
+}
+
+func (c Config) stashedOnly() bool {
+	return c.DocumentTypes.Contains(StashedDoc) && !c.DocumentTypes.Contains(NewsDoc)
+}
+
 // NewProgram returns a new Tea program.
 func NewProgram(cfg Config) *tea.Program {
 	if cfg.Logfile != "" {
