3e612daa98acb855e097da103b477f404e438c10
bashbunni <bunni@bashbunni.dev>
2024-12-03 15:11:42 -0800

    test: skip networked tests if no connection

--- glow_test.go
+++ glow_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"strings"
 	"testing"
 )
 
@@ -16,6 +17,13 @@ func TestGlowSources(t *testing.T) {
 
 	for _, v := range tt {
 		t.Run(v, func(t *testing.T) {
+			// Start by checking for network issues.
+			_, nerr := readmeURL(v)
+			if nerr != nil && strings.Contains(nerr.Error(), "no such host") {
+				t.Logf("Error during execution (args: %s):\n%v", v, nerr)
+				t.Skip("Test uses network. Are you connected to the Internet?")
+			}
+
 			buf := &bytes.Buffer{}
 			err := executeArg(rootCmd, v, buf)
 			if err != nil {
