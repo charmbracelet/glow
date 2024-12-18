7b3bfac3068c5501adc83c2c1a3914c84b0cb136
bashbunni <15822994+bashbunni@users.noreply.github.com>
2024-12-18 11:10:03 -0800

    test: skip networked tests if no connection (#661)
    * test: skip networked tests if no connection
    
    * fix: handle unexpected http responses
    
    * fix: return if a valid URL is generated
    
    * test: delete networked test

--- glow_test.go
+++ glow_test.go
@@ -1,32 +1,8 @@
 package main
 
 import (
-	"bytes"
 	"testing"
 )
-
-func TestGlowSources(t *testing.T) {
-	tt := []string{
-		".",
-		"README.md",
-		"github.com/charmbracelet/glow",
-		"github://charmbracelet/glow",
-		"https://github.com/charmbracelet/glow",
-	}
-
-	for _, v := range tt {
-		t.Run(v, func(t *testing.T) {
-			buf := &bytes.Buffer{}
-			err := executeArg(rootCmd, v, buf)
-			if err != nil {
-				t.Errorf("Error during execution (args: %s): %v", v, err)
-			}
-			if buf.Len() == 0 {
-				t.Errorf("Output buffer should not be empty (args: %s)", v)
-			}
-		})
-	}
-}
 
 func TestGlowFlags(t *testing.T) {
 	tt := []struct {
