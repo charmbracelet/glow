7dc69ab85b0253e3bc9b4d9a81cdbb8ba8c39b0a
Davit <davit@akis.am>
2025-07-18 12:34:31 +0400

    test render

--- flow.go
+++ flow.go
@@ -7,8 +7,18 @@ import (
 	"time"
 
 	"github.com/charmbracelet/glamour"
+	"github.com/charmbracelet/glamour/ansi"
+	"github.com/charmbracelet/glamour/styles"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/spf13/cobra"
+)
+
+type TextPosition int
+
+const (
+	TextStart TextPosition = iota
+	TextMiddle
+	TextEnd
 )
 
 func executeWithFlow(cmd *cobra.Command, src *source, w io.Writer) error {
@@ -19,8 +29,21 @@ func executeWithFlow(cmd *cobra.Command, src *source, w io.Writer) error {
 		n, err := src.reader.Read(b)
 		if err != nil {
 			if err == io.EOF {
-				if n == 0 && nextLine == "" {
-					return nil
+				if n == 0 {
+					if len(line) > 0 {
+						out := renderString(line, "NOT USED NOW", TextStart)
+						if strings.HasPrefix(line, "\n") {
+							fmt.Fprint(w, "\n")
+						}
+						fmt.Fprint(w, "\r"+out)
+						if strings.HasSuffix(line, "\n") {
+							fmt.Fprint(w, "\n")
+						}
+						line = ""
+					}
+					if nextLine == "" {
+						return nil
+					}
 				}
 			} else {
 				return fmt.Errorf("unable to read from reader: %w", err)
@@ -53,90 +76,65 @@ func executeWithFlow(cmd *cobra.Command, src *source, w io.Writer) error {
 			line = fullLine
 		}
 
+		out := renderString(line, "NOT USED NOW", TextStart)
+
 		if strings.HasSuffix(line, "\n") {
-			fmt.Fprint(w, "\r"+line)
+
+			if strings.HasPrefix(line, "\n") {
+				fmt.Fprint(w, "\n")
+			}
+			fmt.Fprint(w, "\r"+out)
+			if strings.HasSuffix(line, "\n") {
+				fmt.Fprint(w, "\n")
+			}
+
 			line = ""
 			continue
 		}
 		if len(line) > int(width) {
 			continue
 		}
-		fmt.Fprint(w, "\r"+line)
+		if strings.HasPrefix(line, "\n") {
+			fmt.Fprint(w, "\n")
+		}
+		fmt.Fprint(w, "\r"+out)
+		if strings.HasSuffix(line, "\n") {
+			fmt.Fprint(w, "\n")
+		}
 	}
 }
 
-func renderString(in string) string {
-	// TODO: Implement flow rendering logic
+func renderString(in string, style string, position TextPosition) string {
+
+	var styleConfig ansi.StyleConfig
+
+	// Get style
+	if lipgloss.HasDarkBackground() {
+		styleConfig = styles.DarkStyleConfig
+	} else {
+		styleConfig = styles.LightStyleConfig
+	}
+
+	m := uint(2)
+	styleConfig.Document.BlockPrefix = ""
+	styleConfig.Document.BlockSuffix = ""
+	styleConfig.Document.Margin = &m
 
 	r, err := glamour.NewTermRenderer(
 		glamour.WithColorProfile(lipgloss.ColorProfile()),
-		glamour.WithAutoStyle(),
-		glamour.WithWordWrap(int(width)), //nolint:gosec
-		glamour.WithBaseURL(baseURL),
-		glamour.WithPreservedNewLines(),
+		glamour.WithStyles(styleConfig),
+		glamour.WithWordWrap(int(0)), //nolint:gosec
 	)
 
 	if err != nil {
-		panic("err on creating renderer")
+		panic("ERRRORRR")
 	}
 
-	
+	out, _ := r.Render(in)
+	out = strings.ReplaceAll(out, "\n", "")
+	return out
+}
 
-	/////////***********************
-
-	// b = utils.RemoveFrontmatter(b)
-
-	// styleOptions := utils.GlamourStyle(style, isCode)
-
-	// // initialize glamour
-	// r, err := glamour.NewTermRenderer(
-	// 	glamour.WithColorProfile(lipgloss.ColorProfile()),
-	// 	styleOptions,
-	// 	glamour.WithWordWrap(int(width)), //nolint:gosec
-	// 	glamour.WithBaseURL(baseURL),
-	// 	glamour.WithPreservedNewLines(),
-	// )
-	// if err != nil {
-	// 	return fmt.Errorf("unable to create renderer: %w", err)
-	// }
-
-	// content := string(b[:n])
-
-	// if isCode {
-	// 	content = utils.WrapCodeBlock(string(b), ext)
-	// }
-	// line = line + string(b[:n])
-	// out, err := r.Render(line)
-	// rend := "\r" + strings.TrimRight(out, "\n")
-	// if err != nil {
-	// 	return fmt.Errorf("unable to render markdown: %w", err)
-	// }
-
-	// // display
-	// switch {
-	// case pager || cmd.Flags().Changed("pager"):
-	// 	pagerCmd := os.Getenv("PAGER")
-	// 	if pagerCmd == "" {
-	// 		pagerCmd = "less -r"
-	// 	}
-
-	// 	pa := strings.Split(pagerCmd, " ")
-	// 	c := exec.Command(pa[0], pa[1:]...) //nolint:gosec
-	// 	c.Stdin = strings.NewReader(out)
-	// 	c.Stdout = os.Stdout
-	// 	if err := c.Run(); err != nil {
-	// 		return fmt.Errorf("unable to run command: %w", err)
-	// 	}
-	// 	return nil
-	// case tui || cmd.Flags().Changed("tui"):
-	// 	path := ""
-	// 	if !isURL(src.URL) {
-	// 		path = src.URL
-	// 	}
-	// 	return runTUI(path, content)
-	// default:
-	// 	if _, err = fmt.Fprint(w, rend); err != nil {
-	// 		return fmt.Errorf("unable to write to writer: %w", err)
-	// 	}
-	// }
+func writeRendered(w io.Writer, line, out string) {
+	// TODO: move write code here
 }
